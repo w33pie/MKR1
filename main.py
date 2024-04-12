289bb5a16f949350677f55f17336690c8b5cc565
Yehor Karpov <yegor.karpo0808@gmail.com>
2024-04-12 15:40:02 +0300

    added calculate_population_change function

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ def read_population_data(file_path: str) -> DataFrame:
     data: DataFrame = pd.read_csv(file_path, header=None, names=['Country', 'Year', 'Population'])
     return data
 
-
+def calculate_population_change(data: DataFrame) -> DataFrame:
+    """Обрахунок зміни населення за роками для кожної країни."""
+    grouped: DataFrame = data.sort_values(by=['Country', 'Year'])
+    grouped['Population Change'] = grouped.groupby('Country')['Population'].diff().fillna(0)
+    return grouped
